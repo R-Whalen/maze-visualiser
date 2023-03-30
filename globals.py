6ab603aad4cb94f0be8d34a826403572e6a199f8
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2023-03-30 06:25:14 +0100

    britainisation

--- globals.py
+++ globals.py
@@ -12,12 +12,12 @@ cells = 50 # changes visible cell count - higher number == increase the number o
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (80, 80, 80)
-START_COLOR = (255, 51, 0)
-END_COLOR = (51, 204, 51)
+START_COLOUR = (255, 51, 0)
+END_COLOUR = (51, 204, 51)
 HOVERED = (0, 102, 0)
-PATH_COLOR = (255, 165, 0)
-EXPAND_COLOR = (130, 232, 130)
-VISITED_COLOR = (179, 249, 255)
+PATH_COLOUR = (255, 165, 0)
+EXPAND_COLOUR = (130, 232, 130)
+VISITED_COLOUR = (179, 249, 255)
 
 # setup fonts
 pygame.font.init()
