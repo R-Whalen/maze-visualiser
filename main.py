376045212bac0b1395bf704268177b76dd4e1105
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2023-03-31 05:46:12 +0100

    fix main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from menu import *
+from display import *
+from globals import *
+from node import *
 
 if __name__ == "__main__":
-    pathfindAlg, mazeGenAlg = menu()
+    pathfindAlg, mazeGenAlg, mazeGen, weight = menu()
