8a774c84252f40d6e12ea9391f7ccf8ba23762b4
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2023-03-31 06:44:10 +0100

    change menu() to mainMenu()

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from globals import *
 from node import *
 
 if __name__ == "__main__":
-    pathfindAlg, mazeGenAlg, mazeGen, weight = menu()
+    pathfindAlg, mazeGenAlg, mazeGen, weight = mainMenu()
