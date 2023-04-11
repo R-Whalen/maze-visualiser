09efd4dde28fc8fe65bf81fec7ce1b9d5c2b46d4
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2023-04-11 04:31:52 +0100

    init pathfinding algorithms

---
+++ algorithms/generation.py
@@ -0,0 +1,16 @@
+from globals import *
+from display import *
+import sys
+
+def recursiveBacktracking():
+    return True
+
+def kruskal(start, end, board, maze):
+    return True
+    
+def eller():
+    print('routing properly')
+    sys.exit()
+    
+def prim():
+    return True    
