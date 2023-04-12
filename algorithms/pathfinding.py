bffbe78f7c2314185d5ce209b47137cbfde6439f
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2023-04-12 04:24:13 +0100

    astar v1

--- algorithms/pathfinding.py
+++ algorithms/pathfinding.py
@@ -1,10 +1,14 @@
-import sys
+from queue import PriorityQueue
+from globals import *
+from display import *
 
 # UTILITY FUNCTIONS
 
 def canMove(current, neighbour):
     # only have to check for current walls as this is accounted for when mazes are being generated
-    
+    # neighbour is an obstacle
+    if neighbour.obstacle == True:
+        return False
     # can move up
     if current.y - neighbour.y == 1 and current.walls[0] is True:
         return False
@@ -21,13 +25,70 @@ def canMove(current, neighbour):
     # returns true if makes it past the if statement gauntlet
     return True
 
+def manhattan(pos1, pos2):
+    # returns manhattan distance between two nodes
+    
+    return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
+
+def findPath(pos):
+    # find the path from a position to the start
+    current = pos
+    path = [current]
+    
+    while current.parent is not None:
+        current = current.parent
+        path.append(current)
+        
+    return path
+
+def buildPath(path, start, end, weighted, board):
+    for node in path[::-1]:
+        node.colour = PATH_COLOUR
+        redrawWindow(start, end, board, weighted)
 
 # MAIN PATHFINDING FUNCTIONS
 
 def astar(start, end, maze, board, weighted):
-    start.distance = 0
+    # manually assign distance from start to start
+    start.distanceFromStart = 0
+    # assign manhattan distance
     visited = set()
-    
+
     entry = 0
     queue = PriorityQueue()
+    queue.put((manhattan(start, end), entry, start))
+    
+    while True:
+        current = queue.get()[2]
+        current.colour = VISITED_COLOUR
+        
+        # formally "visit" current
+        visited.add(current)
+        
+        # exit loop - final act to build the path
+        if current == end:
+            path = findPath(end)
+            buildPath(path, start, end, weighted, board)
+            break
+        
+        for neighbour in current.neighbours:
+            # exclude neighbours we cannot move to
+            if maze is not None and canMove(current, neighbour) is False:
+                continue    
+            
+            temp = current.distanceFromStart + current.weight # weight initialised to 1
+            if temp < neighbour.distanceFromStart and neighbour not in visited:
+                neighbour.parent = current # keep parent parity
+                neighbour.distanceFromStart = temp
+                
+                # A* heuristic, utilises manhattan distance from position and adds it to the score
+                neighbour.distanceToEnd = manhattan(neighbour, end)
+                score = neighbour.distanceToEnd + neighbour.distanceFromStart
+                
+                entry += 1
+                queue.put((score, entry, neighbour))
+        
+        # rerender at the end of each iteration
+        redrawWindow(start, end, board, weighted)
+        
     
