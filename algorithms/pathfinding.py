c424b2484c062dcdada49559a0a91f17ca68b126
Robert Whalen <RobertWhalenBusiness@hotmail.com>
2023-04-12 03:49:24 +0100

    basic util function to check if we can move to a neighbour

--- algorithms/pathfinding.py
+++ algorithms/pathfinding.py
@@ -1,2 +1,33 @@
-def astar():
-    print('routing properly')
+import sys
+
+# UTILITY FUNCTIONS
+
+def canMove(current, neighbour):
+    # only have to check for current walls as this is accounted for when mazes are being generated
+    
+    # can move up
+    if current.y - neighbour.y == 1 and current.walls[0] is True:
+        return False
+    # can move down
+    if current.y - neighbour.y == -1 and current.walls[2] is True:
+        return False
+    # can move left
+    if current.x - neighbour.x == 1 and current.walls[1] is True:
+        return False
+    # can move right
+    if current.x - neighbour.x == -1 and current.walls[3] is True:
+        return False
+    
+    # returns true if makes it past the if statement gauntlet
+    return True
+
+
+# MAIN PATHFINDING FUNCTIONS
+
+def astar(start, end, maze, board, weighted):
+    start.distance = 0
+    visited = set()
+    
+    entry = 0
+    queue = PriorityQueue()
+    
